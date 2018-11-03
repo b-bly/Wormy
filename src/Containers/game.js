48b85b20ea5b5564c8a80d8ad8cecd612e8c1ac0
Brendt Bly <brendt.bly@outlook.com>
2018-11-03 13:08:37 -0500

    Two pieces move one direction.

--- src/Containers/game.js
+++ src/Containers/game.js
@@ -8,19 +8,76 @@ const getDefaultState = () => {
   const middle = (Math.floor(CELLSIZE / 2)) - 1
   const middleX = middle * CELLWIDTH + BORDERWIDTH / 2
   const middleY = middle * CELLHEIGHT + BORDERWIDTH / 2
-
   return {
-    playerPosition: {
-      top: 0,
-      left: middleX,
-
-    },
-    playerTurningDirection: null, // {direction: String, turnThreshold: Number}
-    playerDirection: LEFT,
+    playerState: [
+      { // first item is the head
+        position: {
+          top: 0,
+          left: middleX,
+          direction: LEFT,
+        },
+        turning: []
+      },
+      // 2nd body piece for testing
+      { // second piece (body)
+        position: {
+          top: 0,
+          left: middleX + TOTAL_CELL_WIDTH,
+          direction: LEFT,
+        },
+        turning: []
+      }
+    ],
+    // playerPosition: {
+    //   top: 0,
+    //   left: middleX,
+    // },
+    // playerTurningDirection: null, // {direction: String, turnThreshold: Number}
+    // playerDirection: LEFT,
     playerSpeed: 8,
     score: 0,
   }
 }
+
+// *** More than one piece to body ***
+// need a different state structure
+// the head of the snake could be more than one turn ahead of a body piece
+// So turns need to be stored in an array until all of body has turned
+// How about:
+const playerState = [
+  { // first item is the head
+    position: {
+      top: 0,
+      left: 0,
+      direction: LEFT
+    },
+    turning: [  // should not be array for head?
+      {
+        direction: '',
+        threshold: 0,
+      },
+    ]
+  },
+  { // second piece (body)
+    position: {
+      top: 0,
+      left: 0,
+      threshold: 0,
+    },
+    turning: [
+      {
+        direction: '',
+        threshold: 0,
+      },
+      {  // push upcoming turns to array
+        direction: '',
+        threshold: 0,
+      },
+    ]
+  }
+]
+// With this structure, I just have to adjust the code to loop through the array.
+// And instead of setting turning to null, unshift one turn from the array
 
 const OPPOSITE_DIRECTIONS = {
   UP: DOWN,
@@ -77,119 +134,151 @@ export default class Game extends Component {
   // need to 
 
   updatePlayerPosition = () => {
-    const { playerPosition, playerSpeed, playerPosition: { top, left } } = this.state
-    let { playerTurningDirection, playerDirection } = this.state
-    let thresholdExceeded = false;
+    // const { playerPosition, playerSpeed, playerPosition: { top, left } } = this.state
+    // let { playerTurningDirection, playerDirection } = this.state
+    // let thresholdExceeded = false;
+    let { playerSpeed, playerState } = this.state;
+    const playerStateExample = [
+      { // first item is the head
+        position: {
+          top: 0,
+          left: 0,
+          direction: LEFT,
+        },
+        turning: []
+      },
+    ]
+    // Deal with turning later
     // if player is turning,
-    if (playerTurningDirection !== null) {
-      // if threshold not reached, keep moving in playerDirection
+    // if (playerTurningDirection !== null) {
+    //   // if threshold not reached, keep moving in playerDirection
+    //   switch (playerDirection) {
+    //     case UP:
+    //       // if threshold not reached, keep moving in playerDirection (do nothing)
+    //       if (top <= playerTurningDirection.turnThreshold) {
+    //         // if threshold exceeded,
+    //         // reassign playerDirection so player will move in turningDirection + playerspeed
+    //         playerDirection = playerTurningDirection.direction
+    //         // make sure player is turning in the row or column coords
+    //         playerPosition.top = playerTurningDirection.turnThreshold;
+    //         playerTurningDirection = null
+
+    //       }
+    //       break;
+    //     case RIGHT:
+    //       if (left >= playerTurningDirection.turnThreshold) {
+    //         playerDirection = playerTurningDirection.direction
+    //         playerPosition.left = playerTurningDirection.turnThreshold;
+    //         playerTurningDirection = null
+
+    //       }
+    //       break;
+    //     case DOWN:
+
+    //       if (top >= playerTurningDirection.turnThreshold) {
+    //         playerDirection = playerTurningDirection.direction
+    //         playerPosition.top = playerTurningDirection.turnThreshold;
+    //         playerTurningDirection = null
+
+    //       }
+    //       break;
+    //     case LEFT:
+
+    //       if (left <= playerTurningDirection.turnThreshold) {
+    //         playerDirection = playerTurningDirection.direction
+    //         playerPosition.left = playerTurningDirection.turnThreshold;
+    //         playerTurningDirection = null
+
+    //       }
+    //       break;
+    //   }
+    // }
+    playerState = [...playerState].map((bodyPiece) => {
+      const { direction: playerDirection, top, left } = bodyPiece.position
+      const playerPosition = { ...bodyPiece.position }
+
       switch (playerDirection) {
         case UP:
-          // if threshold not reached, keep moving in playerDirection (do nothing)
-          if (top <= playerTurningDirection.turnThreshold) {
-            // if threshold exceeded,
-            // reassign playerDirection so player will move in turningDirection + playerspeed
-            playerDirection = playerTurningDirection.direction
-            // make sure player is turning in the row or column coords
-            playerPosition.top = playerTurningDirection.turnThreshold;
-            playerTurningDirection = null
-
-          }
+          // wait until next column.  Worm can only go in the columns/rows
+          playerPosition.top -= playerSpeed
           break;
         case RIGHT:
-          if (left >= playerTurningDirection.turnThreshold) {
-            playerDirection = playerTurningDirection.direction
-            playerPosition.left = playerTurningDirection.turnThreshold;
-            playerTurningDirection = null
-
-          }
+          playerPosition.left += playerSpeed
           break;
         case DOWN:
-
-          if (top >= playerTurningDirection.turnThreshold) {
-            playerDirection = playerTurningDirection.direction
-            playerPosition.top = playerTurningDirection.turnThreshold;
-            playerTurningDirection = null
-
-          }
+          playerPosition.top += playerSpeed
           break;
         case LEFT:
-
-          if (left <= playerTurningDirection.turnThreshold) {
-            playerDirection = playerTurningDirection.direction
-            playerPosition.left = playerTurningDirection.turnThreshold;
-            playerTurningDirection = null
-
-          }
+          playerPosition.left -= playerSpeed
           break;
       }
 
-    }
-    switch (playerDirection) {
-      case UP:
-        // wait until next column.  Worm can only go in the columns/rows
-        playerPosition.top -= playerSpeed
-        break;
-      case RIGHT:
-        playerPosition.left += playerSpeed
-        break;
-      case DOWN:
-        playerPosition.top += playerSpeed
-        break;
-      case LEFT:
-        playerPosition.left -= playerSpeed
-        break;
-    }
-
-    // check walls
-    switch (playerDirection) {
-      case UP:
-        if (top <= 0) playerPosition.top = 0;
-        break;
-      case DOWN:
-        if (top >= WINDOWHEIGHT - TOTAL_CELL_HEIGHT) playerPosition.top = WINDOWHEIGHT - TOTAL_CELL_HEIGHT;
-        break;
-      case LEFT:
-        if (left <= 0) playerPosition.left = 0;
-        break;
-      case RIGHT:
-        if (left >= WINDOWWIDTH - TOTAL_CELL_WIDTH) playerPosition.left = WINDOWWIDTH - TOTAL_CELL_WIDTH;
-        break;
-    }
+      // check walls
+      switch (playerDirection) {
+        case UP:
+          if (top <= 0) playerPosition.top = 0;
+          break;
+        case DOWN:
+          if (top >= WINDOWHEIGHT - TOTAL_CELL_HEIGHT) playerPosition.top = WINDOWHEIGHT - TOTAL_CELL_HEIGHT;
+          break;
+        case LEFT:
+          if (left <= 0) playerPosition.left = 0;
+          break;
+        case RIGHT:
+          if (left >= WINDOWWIDTH - TOTAL_CELL_WIDTH) playerPosition.left = WINDOWWIDTH - TOTAL_CELL_WIDTH;
+          break;
+      }
+      return {
+        position: playerPosition,
+        turning: [],
+      }
+    })
     this.setState({
-      playerPosition: playerPosition,
-      playerTurningDirection: playerTurningDirection,
-      playerDirection: playerDirection,
+      playerState: playerState
     })
   }
 
   // player pressing direction keys in worm.js
   handlePlayerMovement = (direction) => {
+    // how to handle a player pressing keys repeatedly before the worm has turned: 
+    // If there's a previous turn for the worm head: replace previous turn with the 
+    // new one. ( for each body piece)
+    // If no turn in queue, then add it to each body piece.
+    let { playerState } = this.state
 
-    // can't go backwards.  Don't register if opposite: if going 'UP', you can't go 'DOWN'
-    let threshold = null
-    let playerTurningDirection = this.state.playerTurningDirection;
-    if (this.state.playerDirection !== OPPOSITE_DIRECTIONS[direction]) {
-      if (direction !== this.state.playerDirection) {
-        // set playerTurningDirection if it is a different direction (ie player didn't hit left twice)
-        threshold = this.getTurnThreshold();
-        playerTurningDirection = {
-          direction: direction,
-          turnThreshold: threshold,
-        };
-        // don't update direction
+    playerState = playerState.map((piece) => {
+
+      let threshold = null
+      let { position: { direction: playerDirection }, turning: { direction: playerTurningDirection } } = piece;
+      // can't go backwards.  Don't register if opposite: if going 'UP', you can't go 'DOWN'
+      if (playerDirection !== OPPOSITE_DIRECTIONS[direction]) {
+        // can't be the same direction
+        if (direction !== playerDirection) {
+          // set playerTurningDirection if it is a different direction (ie player didn't hit left twice)
+          threshold = this.getTurnThreshold();
+          playerTurningDirection = {
+            direction: direction,
+            turnThreshold: threshold,
+          };
+          // don't update direction
+        }
+        this.setState({
+          playerTurningDirection: playerTurningDirection,
+        }, () => {
+          console.log(this.state);
+
+        });
       }
-      this.setState({
-        playerTurningDirection: playerTurningDirection,
-      }, () => {
-        console.log(this.state);
 
-      });
-    }
+    })
   }
 
   getTurnThreshold = () => {
-    const { playerDirection, playerPosition } = this.state;
+    // const { playerDirection, playerPosition } = this.state;
+    // ***placeholders for testing REPLACE!
+    const playerDirection = 'LEFT'
+    const playerPosition = { top: 0, left: 0 }
+
     let threshold
     switch (playerDirection) {
       // player is moving up, and turning left or right.  
@@ -236,14 +325,13 @@ export default class Game extends Component {
 
 
   render() {
-    const {
-      playerPosition
-    } = this.state
 
     return (
       <div>
         <Board />
-        <Worm playerPosition={playerPosition}
+        <Worm playerPosition={this.state.playerState[0].position}
+          handlePlayerMovement={this.handlePlayerMovement} />
+        <Worm playerPosition={this.state.playerState[1].position}
           handlePlayerMovement={this.handlePlayerMovement} />
       </div>
     )
